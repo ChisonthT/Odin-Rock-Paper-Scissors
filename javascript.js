6af1724e33266957a19cab67a2691286b4df2158
Chisonth <chisontht@gmail.com>
2023-05-05 13:45:13 -0400

    Added game function, functionality and took out bugs.

--- javascript.js
+++ javascript.js
@@ -8,45 +8,49 @@ function getComputerChoice(){
 function inputCheck(playerSelection){
     const upPlayerSelection = playerSelection.toUpperCase()
     
-    if (upPlayerSelection != "ROCK" 
-    || upPlayerSelection != "SCISSOR" 
-    || upPlayerSelection != "PAPER"){ 
+    if (upPlayerSelection == "ROCK" || upPlayerSelection == "SCISSOR" || upPlayerSelection == "PAPER"){ 
+        return true
+    } else{
         console.log("Please try one of Rock, Scissor or Paper")
         return false
-    } else{
-        return true
     }
 }
 
-function round(playerSelection, computerSelection){
+function round(playerSelection, computerSelection, playerPoints){
     const upPlayerSelection = playerSelection.toUpperCase()
     const upCompSelection = computerSelection.toUpperCase()
     
     if (upPlayerSelection == upCompSelection) {
         return "It's a tie"
-    } else if (upPlayerSelection == "Rock" && upCompSelection == "Scissor"){
+    } else if (upPlayerSelection == "ROCK" && upCompSelection == "SCISSOR"){
         return "Rock beats Scissor. You win!"
-    } else if (upPlayerSelection == "Scissor" && upCompSelection == "Paper"){
+    } else if (upPlayerSelection == "SCISSOR" && upCompSelection == "PAPER"){
         return "Scissor beats Paper. You win!"
-    } else if (upPlayerSelection == "Paper" && upCompSelection == "Rock"){
+    } else if (upPlayerSelection == "PAPER" && upCompSelection == "ROCK"){        
         return "Paper beats Rock. You win!"
     } else{
-        return "You lose!"
+        return "You lose! " + computerSelection + " beats " + playerSelection 
     }
 }
 
+function game(){
+    let playerPoints = 0
 
-let keepPlaying = true
-while (keepPlaying){
-    computerSelection = getComputerChoice();
-    playerSelection = prompt("Enter your choice of Rock, Paper or Scissor: ");
+    for (let i = 0; i < 5; i++){
+        computerSelection = getComputerChoice();
+        playerSelection = prompt("Enter Rock, Paper, or Scissor: ");
 
-    if (inputCheck(playerSelection)){
-        console.log(round(playerSelection, computerSelection));
-        console.log("Somethjingz`");
+        if (inputCheck(playerSelection)){
+            console.log(playerSelection, computerSelection)
+            outcomeString = round(playerSelection, computerSelection);
+            if (outcomeString.search("win") != -1){
+                playerPoints += 1
+            }
+            console.log(outcomeString);
+        }
+
     }
-
-    if (playerSelection == "Quit"){
-        keepPlaying= false;
-    }
+    console.log("You got " + playerPoints + " out of 5!")
 }
+
+game()
